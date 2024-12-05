cd65750d56b81c7b539f5fa3b85e6b461fa341a4
PedroAugustoNascimento <150451782+PedroAugustoNascimento@users.noreply.github.com>
2024-12-04 22:44:52 -0300

    fazendo novos testes

--- src/Habay.java
+++ src/Habay.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.StringTokenizer;
+
+import javax.swing.SpinnerDateModel;
+
 import java.util.Set;
 import java.util.LinkedHashSet;
+import java.util.Collections;
 
 /*
 Joao NO
@@ -27,7 +33,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<String> nomes = new ArrayList<>();
-        Set<String> nomesOrganizados = new LinkedHashSet<>();
+        List<String> nomesYes = new ArrayList<>();
+        List<String> nomesNo = new ArrayList<>();
+        int maximo = 0;
+        String vencedor = "";
 
         //realizando a entrada de dados
         while (sc.hasNextLine()) {
@@ -43,32 +52,49 @@ public class Main {
         }
         sc.close();
 
-        //salvando em uma lista somente os nomes com YES 
+        //salvando em uma lista os nomes com YES e NO
         for (int i=0; i<nomes.size(); i++){
             String atual = nomes.get(i);
             if(i>0){
                 String anterior = nomes.get(i-1);
                 if(atual.equals("YES")){
-                    nomesOrganizados.add(anterior);
+                    if(!nomesYes.contains(anterior)){
+                        nomesYes.add(anterior);
+                    }
+                } 
+                if (atual.equals("NO")) {
+                    if(!nomesYes.contains(anterior)){
+                        nomesNo.add(anterior);
+                    }
                 }
+                    
+                    
+            }
+        }
+        System.out.println("=================================");
+        for (String nome: nomesYes){
+            System.out.println(nome);
+        }
+
+        for (String nome : nomesYes){
+            if((nome.length() > 0 ) || (nome.length() == maximo && vencedor.isEmpty())){
+                vencedor = nome;
+                maximo = nome.length();
             }
         }
 
-        /*System.out.println("==============================");
-        for (String word : nomesOrganizados) {
-            System.out.println(word);
-        }*/
+        Collections.sort(nomesYes);
+        Collections.sort(nomesNo);
 
-        String[] comparacoes = new String[nomesOrganizados.size()];
-        for (int i=0; i<nomesOrganizados.size(); i++){
-            String word = "";
-            comparacoes[i] = word;
+        for (String nome: nomesYes){
+            System.out.println(nome);
+        }
+        for (String nome: nomesNo){
+            System.out.println(nome);
         }
 
-        System.out.println("==================================");
-        for (int i=0; i<comparacoes.length; i++){
-            System.out.println(comparacoes[i]);
-        }
-
+        System.out.println();
+        System.out.println("Amigo do Habay");
+        System.out.println(vencedor);
     }
 }
